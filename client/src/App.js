9b739cded6e51959bd6baf9a4df5e45364766882
Colby Thomas <coloradocolby@gmail.com>
2019-10-05 01:27:22 -0500

    ui work

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 class App extends Component {
   state = {
     profiles: [],
+    showModal: false,
   }
   async componentDidMount() {
     const response = await axios.get('http://localhost/api/profile')
@@ -14,37 +15,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
+      <div className='App font-sans'>
         <Navbar />
-        <div class='max-w-sm rounded overflow-hidden shadow-lg'>
-          <img
-            class='w-full'
-            src='https://images.unsplash.com/photo-1569834381156-7b735e41e57d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80'
-            alt='Sunset in the mountains'
-          />
-          <div class='px-6 py-4'>
-            <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
-            <p class='text-gray-700 text-base'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class='px-6 py-4'>
-            <span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-              #photography
-            </span>
-            <span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-              #travel
-            </span>
-            <span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-500'>
-              #winter
-            </span>
-          </div>
-        </div>
-        <pre className='bg-purple-300 m-6 p-6 rounded shadow-lg'>
-          {JSON.stringify(this.state.profiles, null, 2)}
-        </pre>
+        <main className='px-4'>
+          {this.state.profiles.map(profile => (
+            <div className='max-w-sm bg-white rounded overflow-hidden shadow-md hover:shadow-lg transition-all transition-100 m-100'>
+              <img
+                className='w-full'
+                src={profile.user.avatar}
+                alt='Sunset in the mountains'
+              />
+              <div className='px-6 py-4'>
+                <div className='font-bold text-xl mb-2'>
+                  {profile.user.name}
+                </div>
+                <p className='text-gray-700 text-base'>{profile.bio}</p>
+              </div>
+              <div className='px-6 py-4'>
+                {profile.disciplines.map(discipline => (
+                  <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+                    #{discipline}
+                  </span>
+                ))}
+              </div>
+            </div>
+          ))}
+
+          {/* <pre className='bg-purple-300 my-4 p-6 rounded shadow-lg hover:bg-indigo-300 transition-all transition-2000'>
+            {JSON.stringify(this.state.profiles, null, 2)}
+          </pre> */}
+        </main>
       </div>
     )
   }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,37 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-  <ul className='flex items-center h-12 bg-blue-400'>
-    <li className='mx-6'>
-      <a className='text-white font-bold text-3xl' href='#'>
-        unehub
-      </a>
-    </li>
-    <li className='mx-6'>
-      <a className='text-white font-bold' href='#'>
-        Profiles
-      </a>
-    </li>
-    <li className='mx-6'>
-      <a className='text-white font-bold' href='#'>
-        Search
-      </a>
-    </li>
-    <li className='mx-6'>
-      <a className='text-white font-bold' href='#'>
-        Link
-      </a>
-    </li>
-    <li className='flex-grow'></li>
-    <li className='mx-6'>
-      <a className='text-white font-bold' href='#'>
-        Sign up
-      </a>
-    </li>
-    <li className='mx-6'>
-      <a className='text-white font-bold' href='#'>
-        Log in
-      </a>
-    </li>
-  </ul>
-)
+class Navbar extends Component {
+  state = {
+    profiles: [],
+    showModal: false,
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.showModal && (
+          <div
+            className='fixed inset-0 z-50 overflow-auto bg-smoke_light flex items-center'
+            onClick={() => this.setState({ showModal: !this.state.showModal })}
+          >
+            <div className='bg-white mx-10 p-5 rounded shadow-lg'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </div>
+          </div>
+        )}
+
+        <nav className='flex items-center justify-between flex-wrap bg-white mb-4 text-gray-800 border-'>
+          <div className='flex items-center flex-shrink-0 mx-3'>
+            <span className='font-semibold text-2xl tracking-loose'>
+              unehub
+            </span>
+          </div>
+          <div className='block lg:hidden'>
+            <button className='flex items-center px-3 py-2 border rounded text-xl border-custom_purple hover:border-white'>
+              <svg
+                className='fill-current h-3 w-3'
+                viewBox='0 0 20 20'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <title>Menu</title>
+                <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+              </svg>
+            </button>
+          </div>
+          <div className='w-full block flex-grow lg:flex lg:w-auto'>
+            <div className='p-4 hover:bg-gray-200 hover:text-blue-500 lg:flex lg:justify-center lg:items-center lg:text-center'>
+              <a
+                href='#responsive-header'
+                className='block mt-4 lg:inline-block lg:mt-0 '
+              >
+                Riders
+              </a>
+            </div>
+            <div className='lg:flex-grow'></div>
+            <div className='flex'>
+              <button
+                className='lg:self-center bg-custom_green rounded px-2 py-1 text-white mt-4 mx-2 lg:mt-0'
+                onClick={() =>
+                  this.setState({ showModal: !this.state.showModal })
+                }
+              >
+                Sign up
+              </button>
+              <button className='lg:self-center bg-gray-200 text-gray-700 rounded px-2 py-1 text-white mt-4 mx-2 lg:mt-0'>
+                Log in
+              </button>
+            </div>
+          </div>
+        </nav>
+      </>
+    )
+  }
+}
+export default Navbar
